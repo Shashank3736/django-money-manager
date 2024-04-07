b0b41b32e4f8459bf0107c7a8c24c4fc908c6d47
Shreyash Raj <shreyashr267@gmail.com>
2024-04-08 03:21:04 +0530

    Create UserSerializer

---
+++ account/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import CustomUser as User
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'id', 'email']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    def update(self, instance, validated_data):
+        super().update(instance, validated_data)
+        password = validated_data.get('password')
+        if password:
+            user = User.objects.get(username=instance.username)
+            user.set_password(password)
+            user.save()
+        return instance
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if self.context['request'].user != instance and not self.context['request'].user.is_admin:
+            representation['email'] = None
+        return representation
