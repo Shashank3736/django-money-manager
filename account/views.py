11171f7032ad3a3ae6e13a06ff19f2379f9232b8
Shreyash Raj <shreyashr267@gmail.com>
2024-04-08 03:21:26 +0530

    Created UserViewSet

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import CustomUser as User
+from .serializers import UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    partial = True
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        user = User.objects.get(id=request.user.id)
+        serializer = self.get_serializer(user, many=False)
+        return Response(serializer.data)
