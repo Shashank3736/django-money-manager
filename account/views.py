9770792ebda6d4183d8d163cba58495ee754a171
Shreyash Raj <shreyashr267@gmail.com>
2024-04-08 23:57:54 +0530

    Take away the power to see all users as it's not required

--- account/views.py
+++ account/views.py
@@ -6,10 +6,14 @@ from .serializers import UserSerializer
 from .permissions import IsOwnerOrReadOnly
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsOwnerOrReadOnly]
     partial = True
+
+    def get_queryset(self):
+        if self.request.user:
+            return User.objects.filter(id=self.request.user.id)
+        return []
 
     @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
     def me(self, request):
