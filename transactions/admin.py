cb3286c2a992f1f00fc4adf5d76930c3d14de841
Shreyash Raj <shreyashr267@gmail.com>
2024-04-08 17:04:49 +0530

    Added models

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Transaction, Account, Category
 # Register your models here.
+
+admin.site.register(Transaction)
+admin.site.register(Account)
+admin.site.register(Category)
