8078a9147f7f39f02e2efe63822b09b7ab89a7df
Shreyash Raj <shreyashr267@gmail.com>
2024-04-09 00:27:09 +0530

    HyperLinked not supported anymore so removed

--- account/serializers.py
+++ account/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser as User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     class Meta:
         model = User
-        fields = ['url', 'username', 'id', 'email', 'password']
+        fields = ['username', 'id', 'email', 'password']
+        
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
