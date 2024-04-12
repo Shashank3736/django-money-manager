5c69f3ed1fe26fd37ea9df4a348b3532d0f33daf
Shreyash Raj <shreyashr267@gmail.com>
2024-04-13 00:06:55 +0530

    Security from incorrect data

--- budget/serializers.py
+++ budget/serializers.py
@@ -9,3 +9,11 @@ class BudgetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Budgets
         fields = "__all__"
+    
+    def create(self, validated_data):
+        if validated_data['category'] and validated_data['category'].user != validated_data['user']:
+            raise ValidationError("Category is not owned by the user.")
+        
+        if validated_data['amount'] < 0:
+            raise ValidationError("Amount need to be greater than zero.")
+        return super().create(validated_data)
