88f63987b5ff26a38592a9a5a5af995c031af33a
Shreyash Raj <shreyashr267@gmail.com>
2024-04-12 23:53:33 +0530

    added viewsets

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, permissions
+from .serializers import BudgetSerializer
 
 # Create your views here.
+class BudgetViewSet(viewsets.ModelViewSet):
+    serializer_class = BudgetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.budgets.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
