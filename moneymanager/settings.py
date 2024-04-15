8564658e59b86a493c341746f87ef18ed1d07767
Shreyash Raj <shreyashr267@gmail.com>
2024-04-15 22:57:49 +0530

    PostgreSQL Migration (2/3) Updated database setting

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -83,12 +83,25 @@ WSGI_APPLICATION = 'moneymanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv("DATABASE_NAME")),
+        'USER': str(os.getenv("DATABASE_USER")),
+        'PASSWORD': str(os.getenv("DATABASE_PASSWORD")),
+        'HOST': str(os.getenv("DATABASE_HOST")),
+        'PORT': str(os.getenv("DATABASE_PORT")),
     }
 }
+##########
 
 
 # Password validation
