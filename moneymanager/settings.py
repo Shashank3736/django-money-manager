e6faa65d0812d7cb4db4fa83c913546425b93ed1
Shreyash Raj <shreyashr267@gmail.com>
2024-04-15 12:32:31 +0530

    Production Read - Load .env - Set Debug to false - Allow localhost

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+# Load dotenv
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Done
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1%sd%uf=x9vo#(@^v(e_15lbs1#fo5=%t($y$*q!9ooy4*j#_e'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
