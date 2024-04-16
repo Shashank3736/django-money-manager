59df9f9ff8ff9a1729cad4edafb8746209429e9d
Shreyash Raj <shreyashr267@gmail.com>
2024-04-16 13:39:15 +0530

    Created Profile model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from account.models import CustomUser as User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
