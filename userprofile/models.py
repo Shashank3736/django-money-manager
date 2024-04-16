3a03ed96a5c645a955a43497c97a3a9e5a3a594c
Shreyash Raj <shreyashr267@gmail.com>
2024-04-16 15:10:11 +0530

    Updated model with UUID for id and made profile to be onetoonefield

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from account.models import CustomUser as User
+import uuid
 
 # Create your models here.
 class Profile(models.Model):
-    profile_pic = models.ImageField(null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    profile_pic = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
