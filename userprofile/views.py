4227ec33a7aff192dc79b7d3d111b0780873a9d0
Shreyash Raj <shreyashr267@gmail.com>
2024-04-16 15:14:28 +0530

    Created rest framework model viewset

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .serializers import ProfileSerializer
+from .models import Profile
 # Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user).all()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
