4f70a5116f848a49bef6b0cd782b84d6839521b4
Shreyash Raj <shreyashr267@gmail.com>
2024-04-19 00:20:37 +0530

    Replaced with CustomSeriralizer

--- budget/serializers.py
+++ budget/serializers.py
@@ -1,22 +1,11 @@
 from rest_framework import serializers
+from moneymanager.serializers import CustomModelSerializer
 from .models import Budgets
-from rest_framework.exceptions import ValidationError
 
 # code
-class BudgetSerializer(serializers.ModelSerializer):
+class BudgetSerializer(CustomModelSerializer):
     user = serializers.ReadOnlyField(source="user.username")
 
     class Meta:
         model = Budgets
         fields = "__all__"
-    
-    def create(self, validated_data):
-        if validated_data['category'] and validated_data['category'].user != validated_data['user']:
-            raise ValidationError("Category is not owned by the user.")
-        
-        if validated_data['amount'] < 0:
-            raise ValidationError("Amount need to be greater than zero.")
-        try:
-            return Budgets.objects.create(**validated_data)
-        except Exception as e:
-            raise ValidationError(e)
