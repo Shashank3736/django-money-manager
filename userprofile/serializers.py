b29177e1845f151bce6ac0c3890fbe9880cb5fab
Shreyash Raj <shreyashr267@gmail.com>
2024-04-19 00:21:08 +0530

    Removed Hyperlinked and change user representation

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from moneymanager.serializers import CustomModelSerializer
 from .models import Profile
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer, CustomModelSerializer):
-    user = serializers.ReadOnlyField(source='user.pk')
+class ProfileSerializer(CustomModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Profile
