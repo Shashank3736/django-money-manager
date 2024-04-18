718eb373d097c4090ba4654e7470d4a013d0fb4d
Shreyash Raj <shreyashr267@gmail.com>
2024-04-18 21:16:03 +0530

    delete old image if added new

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from account.models import CustomUser as User
 import uuid, os
@@ -15,3 +15,18 @@ def delete_image(sender, instance, **kwargs):
     if instance.profile_pic:
         if os.path.isfile(instance.profile_pic.path):
             os.remove(instance.profile_pic.path)
+
+@receiver(pre_save, sender=Profile)
+def delete_old_image(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            # Retrieve the existing instance from the database
+            old_instance = sender.objects.get(pk=instance.pk)
+            # Check if the image field has changed
+            if old_instance.profile_pic != instance.profile_pic:
+                # Delete the old profile_pic file from the storage
+                if old_instance.profile_pic:
+                    if os.path.isfile(old_instance.profile_pic.path):
+                        os.remove(old_instance.profile_pic.path)
+        except sender.DoesNotExist:
+            pass
