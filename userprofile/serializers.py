d69622553f152df16d4d9b2f1c13a3a7f3c9d686
Shreyash Raj <shreyashr267@gmail.com>
2024-04-18 23:47:37 +0530

    Updated with hyperlinked and custom serializer

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,19 +1,10 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from moneymanager.serializers import CustomModelSerializer
 from .models import Profile
 
-class ProfileSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+class ProfileSerializer(serializers.HyperlinkedModelSerializer, CustomModelSerializer):
+    user = serializers.ReadOnlyField(source='user.pk')
 
     class Meta:
         model = Profile
         fields = '__all__'
-    
-    def create(self, validated_data):
-        if validated_data['profile_pic']:
-            if validated_data['profile_pic'].size > 1024*1024:
-                raise ValidationError("File is too large")
-        try:
-            return Profile.objects.create(**validated_data)
-        except Exception as e:
-            raise ValidationError(e)
