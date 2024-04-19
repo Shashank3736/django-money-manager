85b5b0feb972b3832ed7e381bd2b1dd727fd5043
Shreyash Raj <shreyashr267@gmail.com>
2024-04-19 18:10:16 +0530

    Removed not important create method

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
-from moneymanager.serializers import CustomModelSerializer
+from moneymanager.serializers import CustomModelSerializer, CustomReadOnlyField
 from .models import Account, Transaction, Category
 from rest_framework.exceptions import ValidationError
 from django.utils import timezone
 
 class AccountSerializer(CustomModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = CustomReadOnlyField()
 
     class Meta:
         model = Account
         fields = '__all__'
 
 class CategorySerializer(CustomModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = CustomReadOnlyField()
     class Meta:
         model = Category
         fields = '__all__'
@@ -20,13 +20,13 @@ class CategorySerializer(CustomModelSerializer):
 class CustomChoiceField(serializers.ChoiceField):
     def to_representation(self, obj):
         return {
-            "id": obj.pk,
+            "_id": obj.pk,
             "name": str(obj)
         }
     
 class TransactionSerializer(CustomModelSerializer):
     category = CustomChoiceField(choices=[], allow_blank=True, allow_null=True)
-    user = serializers.ReadOnlyField(source='user.username')
+    user = CustomReadOnlyField()
     account = CustomChoiceField(choices=[])
     datetime = serializers.DateTimeField(default=timezone.now())
 
@@ -41,24 +41,4 @@ class TransactionSerializer(CustomModelSerializer):
             self.fields['category'].choices = Category.objects.filter(user=request.user).values_list('id', 'name')
             self.fields['account'].choices = Account.objects.filter(user=request.user).values_list('id', 'name')
     
-    def validate(self, data):
-        if data.get('category', None):
-            data['category'] = Category.objects.get(id=data['category'])
-        else:
-            data['category'] = None
-            if data['type'] == 'expense':
-                raise ValidationError({'category': 'Category is required for expense transactions'})
-        if data['account']:
-            data['account'] = Account.objects.get(id=data['account'])
-        
-        if data['amount'] < 0:
-            raise ValidationError({'amount': 'Amount cannot be negative'})
-        
-        if data.get('category', None) and data['category'].user != self.context.get('request').user:
-            raise ValidationError({'category': 'Category does not belong to user'})
-        
-        if data['account'].user != self.context.get('request').user:
-            raise ValidationError({'account': 'Account does not belong to user'})
-        
-        return data
         
