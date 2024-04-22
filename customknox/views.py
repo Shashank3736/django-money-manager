26b8a01c860fb9f5e01cd29544150de387034752
Shreyash Raj <shreyashr267@gmail.com>
2024-04-22 22:20:16 +0530

    allow logoutall to support basic authentication

--- customknox/views.py
+++ customknox/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from knox.views import LogoutAllView as KnoxLogoutAllView
+from knox.auth import TokenAuthentication
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
+class LogoutAllView(KnoxLogoutAllView):
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
