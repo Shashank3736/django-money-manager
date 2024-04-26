1d49ab7816e3ebc1a2f0d5cdcdc1ef69220e67dd
Shreyash Raj <shreyashr267@gmail.com>
2024-04-26 22:46:36 +0530

    Improved representation

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -3,6 +3,7 @@ from moneymanager.serializers import CustomModelSerializer, CustomReadOnlyField
 from .models import Account, Transaction, Category
 from rest_framework.exceptions import ValidationError
 from django.utils import timezone
+import uuid
 
 class AccountSerializer(CustomModelSerializer):
     user = CustomReadOnlyField()
@@ -16,11 +17,26 @@ class CategorySerializer(CustomModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class CustomPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_representation(self, value):
+        try:
+            value = uuid.UUID(str(value))
+            account = self.queryset.get(pk=value)
+            # Customize the representation of the related object here
+            return {
+                '_id': account.id,
+                'name': account.name,
+                # Add more fields as needed
+            }
+        except:
+            # Handle the case where the related object does not exist
+            return None 
     
 class TransactionSerializer(CustomModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.none(), allow_null=True)
+    category = CustomPrimaryKeyRelatedField(queryset=Category.objects.none(), allow_null=True)
     user = CustomReadOnlyField()
-    account = serializers.PrimaryKeyRelatedField(queryset=Account.objects.none())
+    account = CustomPrimaryKeyRelatedField(queryset=Account.objects.none())
     datetime = serializers.DateTimeField(default=timezone.now())
 
     class Meta:
